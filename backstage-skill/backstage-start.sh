740dfccb39b127dc361bb66b538796656efc1c36
Nicholas Frota <hi@nonlinear.nyc>
2026-02-12 21:26:40 -0500

    Fix regex syntax error in backstage-start.sh (escape parentheses in bash regex)

--- backstage-skill/backstage-start.sh
+++ backstage-skill/backstage-start.sh
@@ -36,13 +36,13 @@ read_navigation_block() {
             fi
         elif [[ $in_block -eq 1 ]]; then
             # Parse markdown links: [TEXT](path)
-            if [[ "$line" =~ ROADMAP.*\(([^)]+)\) ]]; then
+            if [[ "$line" =~ ROADMAP.*'('[^')']+')'  ]]; then
                 roadmap_path="${BASH_REMATCH[1]}"
-            elif [[ "$line" =~ CHANGELOG.*\(([^)]+)\) ]]; then
+            elif [[ "$line" =~ CHANGELOG.*'('[^')']+')'  ]]; then
                 changelog_path="${BASH_REMATCH[1]}"
-            elif [[ "$line" =~ CHECKS.*\(([^)]+)\) ]] || [[ "$line" =~ HEALTH.*\(([^)]+)\) ]]; then
+            elif [[ "$line" =~ CHECKS.*'('[^')']+')'  ]] || [[ "$line" =~ HEALTH.*'('[^')']+')'  ]]; then
                 health_path="${BASH_REMATCH[1]}"
-            elif [[ "$line" =~ POLICY.*\(([^)]+)\) ]]; then
+            elif [[ "$line" =~ POLICY.*'('[^')']+')'  ]]; then
                 policy_path="${BASH_REMATCH[1]}"
             fi
         fi
